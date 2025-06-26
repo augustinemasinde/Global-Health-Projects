3985a6df34e223f223eedd3b5687130a44d8a315
Augustine Masinde <augustinemasinde7@gmail.com>
2025-06-26 19:55:08 +0200

    additional data cleaning commands

--- test2.R
+++ test2.R
@@ -1,1 +1,13 @@
-fff
+install.packages("pacman")
+pacman::p_load(
+  rio,        # importing data  
+  here,       # relative file pathways  
+  janitor,    # data cleaning and tables
+  lubridate,  # working with dates
+  matchmaker, # dictionary-based cleaning
+  epikit,     # age_categories() function
+  tidyverse   # data management and visualization
+)
+linelist_raw<- linelist_raw
+skimr::skim(linelist_raw)
+linelist <- linelist %>% rename(genz = 2)
