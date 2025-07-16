8d656d2cecd3ee2ae6043749e28cf64b5b62745e
Augustine Masinde <augustinemasinde7@gmail.com>
2025-07-16 09:12:01 +0200

    new project

--- test2.R
+++ test2.R
@@ -8,6 +8,18 @@ pacman::p_load(
   epikit,     # age_categories() function
   tidyverse   # data management and visualization
 )
+library(janitor)
+library(tidyverse)
 linelist_raw<- linelist_raw
 skimr::skim(linelist_raw)
 linelist <- linelist %>% rename(genz = 2)
+colnames(linelist)
+linelist %>% tabyl(gender, )
+linelist %>% 
+  count(outcome, age_unit, name = "Number", na.rm = T)
+
+table(fct_explicit_na(linelist$gender), fct_explicit_na(linelist$outcome)) %>% 
+  addmargins() %>% 
+  as.data.frame.matrix() %>% 
+  tibble::rownames_to_column(var = "Age Category") %>% 
+  flextable::flextable()
